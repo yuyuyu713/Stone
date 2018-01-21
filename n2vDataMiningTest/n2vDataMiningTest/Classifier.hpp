45e7c441ef4d4b4587e5bcb85404faa522d0d5ab
zhongqi <zhongqi@zqykj.com>
2018-01-21 16:42:38 +0800

    删除无效代码

--- n2vDataMiningTest/n2vDataMiningTest/Classifier.hpp
+++ n2vDataMiningTest/n2vDataMiningTest/Classifier.hpp
@@ -14,27 +14,6 @@ template<typename T> string toStdString(const T& t) {
 
 
 //构建属性结构,读取txt文件中的字符串  
-//struct TupleTitle {
-//	string attr1;
-//	string attr2;
-//	string attr3;
-//	string attr4;
-//	string attr5;
-//
-//	string toString()
-//	{
-//		string str = attr1 + "\t";
-//		str.append(attr2 + "\t")
-//			.append(attr3 + "\t")
-//			.append(attr4 + "\t")
-//			.append(attr5);
-//
-//		return str;
-//	}
-//};
-
-
-// 
 struct _TupleTitle
 {
 	std::vector<int>  attrType; // 0:string, 1:float
@@ -47,27 +26,11 @@ struct _TupleTitle
 		{
 			str.append(item + "\t");
 		}
-
-		//size_t sz = attrName.size();
-		//for (size_t i=0; i<sz; ++i)
-		//{
-		//	if(0 == attrType[i])
-		//	{
-		//		str.append(attrName[i] + "\t");
-		//	}
-		//	else if (1 == attrType[i])
-		//	{
-		//		str.append(attrName[i] + "\t");
-		//	}
-		//	else
-		//	{
-		//		assert(false);
-		//	}
-		//}
 		return str;
 	}
 };
 
+////构建属性结构，将数据转换为所需要的数据以及字符串  
 struct _TupleData
 {
 	long id_l;
@@ -188,103 +151,6 @@ struct _TupleData
 	}
 };
 
-////构建属性结构，将数据转换为所需要的数据以及字符串  
-//struct TupleData {
-//	float attr1;
-//	float attr2;
-//	float attr3;
-//	float attr4;
-//	string attr5;
-//
-//	bool isExsist(string strColumnName) const
-//	{
-//		if (0 == strColumnName.compare("attr1"))
-//		{
-//			return true;
-//		}
-//		else if (0 == strColumnName.compare("attr2"))
-//		{
-//			return true;
-//		}
-//		else if (0 == strColumnName.compare("attr3"))
-//		{
-//			return true;
-//		}
-//		else if (0 == strColumnName.compare("attr4"))
-//		{
-//			return true;
-//		}
-//
-//		return (false);
-//	}
-//
-//	float getColumnValue(string strColumnName) const
-//	{
-//		if (0 == strColumnName.compare("attr1"))
-//		{
-//			return attr1;
-//		}
-//		else if (0 == strColumnName.compare("attr2"))
-//		{
-//			return attr2;
-//		}
-//		else if (0 == strColumnName.compare("attr3"))
-//		{
-//			return attr3;
-//		}
-//		else if (0 == strColumnName.compare("attr4"))
-//		{
-//			return attr4;
-//		}
-//
-//		assert(false);
-//
-//		return -1.0f;
-//	}
-//
-//	void setColumnValue(string strColumnName, float value)
-//	{
-//		if (0 == strColumnName.compare("attr1"))
-//		{
-//			attr1 = value;
-//		}
-//		else if (0 == strColumnName.compare("attr2"))
-//		{
-//			attr2 = value;
-//		}
-//		else if (0 == strColumnName.compare("attr3"))
-//		{
-//			attr3 = value;
-//		}
-//		else if (0 == strColumnName.compare("attr4"))
-//		{
-//			attr4 = value;
-//		}
-//	}
-//
-//	string getLabel() const
-//	{
-//		return attr5;
-//	}
-//
-//	void setLabel(string strLabel)
-//	{
-//		attr5 = strLabel;
-//	}
-//
-//	string toString() const
-//	{
-//		string str = toStdString(attr1) + "\t";
-//		str.append(toStdString(attr2) + "\t")
-//			.append(toStdString(attr3) + "\t")
-//			.append(toStdString(attr4) + "\t")
-//			.append(attr5);
-//
-//		return str;
-//	}
-//};
-
-
 int readDataFile(_TupleTitle &outTitle, vector<_TupleData> &outVecTupleData, const char *fileName, bool bIsContainsTitle)
 {
 	if (nullptr == fileName)
@@ -325,7 +191,7 @@ int readDataFile(_TupleTitle &outTitle, vector<_TupleData> &outVecTupleData, con
 
 		++lineCount;
 
-		std::cout << title.toString() << std::endl;
+		//std::cout << title.toString() << std::endl;
 	}
 	
 	float value = 0.0f;
@@ -360,7 +226,7 @@ int readDataFile(_TupleTitle &outTitle, vector<_TupleData> &outVecTupleData, con
 		vecTupleData.push_back(tupleData);
 
 		++lineCount;
-		std::cout << tupleData.toString() << std::endl;
+		//std::cout << tupleData.toString() << std::endl;
 	}
 
 	infile.close();
@@ -392,26 +258,19 @@ public:
 
 private:
 	// 计算列属性的中位数和绝对标准差
-	//void computeMedianDeviation(string strColumn);
+	void _computeMedianDeviation(string strColumn);
 
 	// 计算单个值的标准分数(使用测试数据生成的中位数和绝对标准差)
 	// 测试或预测时使用
-	//void normalizeTupleData(TupleData &tupleData);
 	void _normalizeTupleData(_TupleData &tupleData);
 
 	_TupleData nearestNeighbor(_TupleData &data);
 
 	float manhattanDistance(_TupleData &data1, _TupleData &data2);
 	float euclideanDistance(_TupleData &data1, _TupleData &data2);
-
-
-	// 
-	void _computeMedianDeviation(string strColumn);
+	
 
 public:
-	//TupleTitle m_title;
-	//vector<TupleData> m_vecTupleData;
-
 	_TupleTitle m_title_;
 	vector<_TupleData> m_vecTupleData_;
 
@@ -419,8 +278,6 @@ public:
 	// column mean's property.
 	map<string, pair<float, float> > m_mapMedianDeviation;
 };
-
-//static string S_ColumnNames[] = { "attr1", "attr2", "attr3", "attr4"};
 
 Classifier::Classifier()
 {
@@ -438,54 +295,9 @@ Classifier::~Classifier()
 
 }
 
-//void Classifier::computeMedianDeviation(string strColumn)
-//{
-//	size_t size = m_vecTupleData.size();
-//	vector<float> columnValue;
-//
-//	for (size_t i = 0; i < size; ++i)
-//	{
-//		TupleData &data = m_vecTupleData[i];
-//
-//		if (data.isExsist(strColumn))
-//		{
-//			columnValue.push_back(data.getColumnValue(strColumn));
-//		}
-//	}
-//
-//	float median = getMedian(columnValue);
-//	float asd = getAbsoluteStandardDeviation(columnValue, median);
-//	
-//	m_mapMedianDeviation[strColumn] = make_pair(median, asd);
-//
-//	//cout << strColumn <<"\tMedian: " << median << "\tASD: " << (asd) << endl;
-//}
-
-//void Classifier::training()
-//{
-//	// S_ColumnNames: attr1,attr3,attr3,attr4,attr5
-//
-//	//1. 计算中位数和绝对标准差
-//	// TODO:
-//	int size = sizeof(S_ColumnNames) - sizeof(string);
-//	for (size_t i = 0; i < 4; ++i)
-//	{
-//		string strColumnName = S_ColumnNames[i];
-//		computeMedianDeviation(strColumnName);
-//	}
-//
-//	//2. 为每个training-data计算标准分数
-//	size = m_vecTupleData.size();
-//	for (size_t i = 0; i < size; ++i)
-//	{
-//		TupleData &data = m_vecTupleData[i];
-//		normalizeTupleData(data);
-//	}
-//}
 
 void Classifier::_computeMedianDeviation(string strColumn)
 {
-	// strColumn: attr1,attr3,attr3,attr4,attr5
 	size_t size = m_vecTupleData_.size();
 	vector<float> columnValue;
 
@@ -546,23 +358,6 @@ void Classifier::_normalizeTupleData(_TupleData &tupleData)
 
 	//cout << "normalizeTupleData: " << data.toString() << endl;
 }
-
-// 计算单个值的标准分数(使用测试数据生成的中位数和绝对标准差)
-//void Classifier::normalizeTupleData(TupleData &tupleData)
-//{
-//	TupleData &data = tupleData;
-//	for(size_t i=0; i< S_ColumnNames->size(); ++i)
-//	{
-//		if (data.isExsist(S_ColumnNames[i]))
-//		{
-//			// 改进后的标准分数 = （（每个值） - （中位数）） / （绝对标准差）
-//			float normalizeValue = (data.getColumnValue(S_ColumnNames[i]) - m_mapMedianDeviation[S_ColumnNames[i]].first) / m_mapMedianDeviation[S_ColumnNames[i]].second;
-//			data.setColumnValue(S_ColumnNames[i], normalizeValue);
-//		}
-//	}
-//
-//	//cout << "normalizeTupleData: " << data.toString() << endl;
-//}
 
 float Classifier::manhattanDistance(_TupleData &data1, _TupleData &data2)
 {
